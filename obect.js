f30c3703227623a672a11ae21fc7f61ab20fb0c4
madson <mr6227873@gmail.com>
2022-01-26 19:36:59 -0200

    primero commit

---
+++ obect.js
@@ -0,0 +1,101 @@
+const videos =[
+  
+    {
+       title:" Lançamento - PARKER Filme com Jason Statham DUBLADO  ",
+       duration:"1:58:40 ",
+       thumb:"img/img1.jpg",
+       video_id:"ZpXUCJHRzYA"
+    },
+     
+ 
+    
+    {
+      title:"Maligna - Filme Completo Dublado - Filme de Terror",
+      duration:"1:13:41",
+      thumb:"img/img2.jpg",
+      video_id:"81EV-tXfkEY"
+    },
+   
+
+    {
+      title:"A NEGOCIADORA FILMES DE AÇÃO FILMES DE AÇÃO 2022 ",
+      duration:"1:25:47",
+      thumb:"img/img3.jpg",
+      video_id:"UaWMX1y-fco"
+    },
+     
+
+    {
+      title:"Diablo - A Última Corrida // Filme Completo Dublado",
+      duration:"1:33:05",
+      thumb:"img/img4.jpg",
+      video_id:"tknLaJ9t5Lk"
+    },
+
+   
+    {
+      title:"Nascido para Correr - Filme Completo Dublado",
+      duration:"1:36:08",
+      thumb:"img/img5.jpg",
+      video_id:"fWWLUaJV5vU"
+    },
+
+
+      
+    {
+      title:"Maldita Aranha Assassina - Filme Completo Dublado",
+      duration:"1:17:02",
+      thumb:"img/img6.jpg",
+      video_id:"FrjyLe7B0gI"
+    },
+
+
+    
+    {
+      title:"lutador de rua completo",
+      duration:"1:46:38",
+      thumb:"img/img7.jpg",
+      video_id:"Cy9g_qU25AI"
+    },
+
+   
+    {
+      title:"o rei dos kickboxers dublado",
+      duration:"1:33:31",
+      thumb:"img/img8.jpg",
+      video_id:"gpdalRUOX5g"
+    },
+
+    
+    {
+      title:"Guardiões Do Sol Dublado 1990 ",
+      duration:"1:26:50",
+      thumb:"img/img9.jpg",
+      video_id:"BulOc1wtrIs"
+    },
+
+
+    {
+      title:"missao quase impossivel",
+      duration:"1:34:12",
+      thumb:"img/img10.jpg",
+      video_id:"w4Ms2_YGsS8"
+    },
+
+
+    {
+      title:"THE KING OF FIGHTERS",
+      duration:"1:32:55",
+      thumb:"img/img11.jpg",
+      video_id:"3DM9--ljEcQ"
+    },
+
+
+    {
+      title:"Vingança Implacável",
+      duration:"1:34:42",
+      thumb:"img/img12.jpg",
+      video_id:"SNpqh0vL81g"
+    }
+
+];
